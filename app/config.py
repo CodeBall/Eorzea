aa249d07847f88a2da6af26ea0ff5e8660259a43
zerol <m@yanzl.net>
2017-05-18 20:25:14 +0800

    change config

--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ SITE_NAME = 'Eorzea'
 ENV_SYMBOL_NAME = 'EORZEA'
 
 
-class base_config(object):
+class BaseConfig(object):
     DEBUG = True
     SECRET_KEY = os.environ.get('SECRET_KEY',
                                 '\x1d\x00-!\x94\xaf;\x01\x84\x86\xab\x91\x8f\xc3qh\xb6\xcd+\x86\x82\xc4\xf1\xd2')
@@ -14,16 +14,16 @@ class base_config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 
-class dev_config(base_config):
+class DevConfig(BaseConfig):
     pass
 
 
-class production_config(base_config):
+class ProductionConfig(BaseConfig):
     pass
 
 configs = {
-    "dev": dev_config,
-    "production": production_config,
+    "dev": DevConfig,
+    "production": ProductionConfig,
 }
 
 config = configs.get(os.environ.get(ENV_SYMBOL_NAME, 'dev'))
