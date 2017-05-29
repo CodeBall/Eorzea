3a838370b97850ebb5006add170ae4a44ce02106
junnplus <junnplus@gmail.com>
2017-05-29 19:30:25 +0800

    add item and item comment model

--- eorzea/models/__init__.py
+++ eorzea/models/__init__.py
@@ -1,5 +1,7 @@
 from eorzea.models.user import UserModel
 from eorzea.models.role import RoleModel
 from eorzea.models.category import CategoryModel
+from eorzea.models.item import ItemModel
+from eorzea.models.item import ItemCommentModel
 
-__all__ = [UserModel, RoleModel, CategoryModel]
+__all__ = [UserModel, RoleModel, CategoryModel, ItemModel, ItemCommentModel]
---
+++ eorzea/models/item.py
@@ -0,0 +1,28 @@
+from eorzea.extensions import db
+
+
+class ItemModel(db.Model):
+    __tablename__ = 'item'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(20))
+    description = db.Column(db.String(512))
+    images = db.Column(db.String(1024), default='')
+    location = db.Column(db.String(128))
+
+    user_id = db.Column(db.Integer, index=True, nullable=False)
+    category_id = db.Column(db.Integer, index=True)
+
+    created_at = db.Column(db.DateTime, server_default=db.func.current_timestamp())
+    updated_at = db.Column(db.DateTime, server_default=db.func.current_timestamp())
+
+
+class ItemCommentModel(db.Model):
+    __tablename__ = 'items_comments'
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200))
+    created_at = db.Column(db.DateTime, server_default=db.func.current_timestamp())
+
+    item_id = db.Column(db.Integer, index=True, nullable=False)
+    user_id = db.Column(db.Integer, index=True, nullable=False)
